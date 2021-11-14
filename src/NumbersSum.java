a69eed1569bc9b4fc544e0c803a0c837a691c984
Sedukhina <vip.seduhina@ukr.net>
2021-11-15 00:59:40 +0200

    Step 4

--- src/NumbersSum.java
+++ src/NumbersSum.java
@@ -7,7 +7,14 @@ public class NumbersSum {
 
     int Add(){
         int sum = 0;
-        String[] numbersSeparated = numbers.split(",");
+        String delimiter = ",";
+        if(numbers != numbers.replaceAll("//.+\\\\n", "")){
+            //delimiter = Character.toString(numbers.charAt(2));
+            int x = numbers.indexOf("n");
+            delimiter = numbers.substring(2,x-1);
+            numbers = numbers.substring(x+1, numbers.length());
+        }
+        String[] numbersSeparated = numbers.split(delimiter);
         for(String numbers2 : numbersSeparated) {
             String[] numbersSeparated2 = numbers2.split("\\\\n");
             for(String number : numbersSeparated2) {
