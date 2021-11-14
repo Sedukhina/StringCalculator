a2d67e8f9032d39b1f975b040b062403f499f1df
Sedukhina <vip.seduhina@ukr.net>
2021-11-15 01:24:47 +0200

    Step 5

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Input your numbers");
-        Scanner in = new Scanner(System.in);
-        String numbers = in.nextLine();
-        NumbersSum ns = new NumbersSum(numbers);
-        int result = ns.Add();
-        System.out.println("Your sum "+Integer.toString(result));
+        try {
+            System.out.println("Input your numbers");
+            Scanner in = new Scanner(System.in);
+            String numbers = in.nextLine();
+            NumbersSum ns = new NumbersSum(numbers);
+            int result = ns.Add();
+            System.out.println("Your sum " + Integer.toString(result));
+        }
+        catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
     }
 }
 
--- src/NumbersSum.java
+++ src/NumbersSum.java
@@ -5,11 +5,11 @@ public class NumbersSum {
         this.numbers = numbers;
     }
 
-    int Add(){
+    int Add() throws Exception{
         int sum = 0;
+        String negatives = "";
         String delimiter = ",";
         if(numbers != numbers.replaceAll("//.+\\\\n", "")){
-            //delimiter = Character.toString(numbers.charAt(2));
             int x = numbers.indexOf("n");
             delimiter = numbers.substring(2,x-1);
             numbers = numbers.substring(x+1, numbers.length());
@@ -18,8 +18,14 @@ public class NumbersSum {
         for(String numbers2 : numbersSeparated) {
             String[] numbersSeparated2 = numbers2.split("\\\\n");
             for(String number : numbersSeparated2) {
+                if (Integer.parseInt(number)<0){
+                    negatives += number + " ";
+                }
                 sum += Integer.parseInt(number);
             }
+        }
+        if(negatives != "") {
+            throw new Exception("Negatives not allowed " + negatives);
         }
         return sum;
     }
