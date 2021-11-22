7bd9777a0b4331b79231564b2fac9e87d9120e8f
Sedukhina <vip.seduhina@ukr.net>
2021-11-22 06:22:57 +0200

    Step 7-9

--- src/NumbersSum.java
+++ src/NumbersSum.java
@@ -5,29 +5,46 @@ public class NumbersSum {
         this.numbers = numbers;
     }
 
-    int Add() throws Exception{
+    int Add() throws Exception {
         int sum = 0;
         String negatives = "";
-        String delimiter = ",";
-        if(numbers != numbers.replaceAll("//.+\\\\n", "")){
-            int x = numbers.indexOf("n");
-            delimiter = numbers.substring(2,x-1);
-            numbers = numbers.substring(x+1, numbers.length());
-        }
-        String[] numbersSeparated = numbers.split(delimiter);
-        for(String numbers2 : numbersSeparated) {
-            String[] numbersSeparated2 = numbers2.split("\\\\n");
-            for(String number : numbersSeparated2) {
-                if (Integer.parseInt(number)<0){
-                    negatives += number + " ";
+        String delimiters = ",";
+        if (numbers != numbers.replaceAll("//.+\\\\n", "")) {
+            int x = numbers.indexOf("\\n");
+            delimiters = numbers.substring(2, x);
+            numbers = numbers.substring(x + 2, numbers.length());
+            if (delimiters.substring(0, 1).equals("[") && delimiters.substring(delimiters.length() - 1, delimiters.length()).equals("]")) {
+                String[] delimitersMassive = delimiters.split("\\[");
+                for (String delim : delimitersMassive) {
+                    if (!delim.equals("") && !delim.substring(delim.length() - 1, delim.length()).equals("]")) {
+                        delimitersMassive[java.util.Arrays.asList(delimitersMassive).indexOf(delim) + 1] = delim + "[" + delimitersMassive[java.util.Arrays.asList(delimitersMassive).indexOf(delim) + 1];
+                    }
+                    else if (!delim.equals("")) {
+                        while (numbers.contains(delim.substring(0, delim.length() - 1))) {
+                            numbers = numbers.replace(delim.substring(0, delim.length() - 1), ",");
+                        }
+                    }
                 }
-                if (Integer.parseInt(number)<1001) {
-                    sum += Integer.parseInt(number);
+            } else {
+                while (numbers.contains(delimiters)) {
+                    numbers = numbers.replace(delimiters, ",");
                 }
             }
-        }
-        if(negatives != "") {
-            throw new Exception("Negatives not allowed " + negatives);
+            String[] numbersSeparated = numbers.split("\\\\n");
+            for (String numbers2 : numbersSeparated) {
+                String[] numbersSeparated2 = numbers2.split(",");
+                for (String number : numbersSeparated2) {
+                    if (Integer.parseInt(number) < 0) {
+                        negatives += number + " ";
+                    }
+                    if (Integer.parseInt(number) < 1001) {
+                        sum += Integer.parseInt(number);
+                    }
+                }
+            }
+            if (negatives != "") {
+                throw new Exception("Negatives not allowed " + negatives);
+            }
         }
         return sum;
     }
